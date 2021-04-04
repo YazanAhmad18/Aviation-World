23236652841d563b9db6bad782f02fa03c494461
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-04-04 16:13:46 +0300

    some change

---
+++ JavaScript/app.js
@@ -0,0 +1,16 @@
+let fullName = prompt('what is your name');
+console.log(fullName);
+console.log(typeof fullName)
+let age = prompt('what is your age');
+console.log(age);
+age= parseInt(age);
+console.log(typeof age)
+
+
+let airline=prompt('pleas chose your favourite airline company');
+console.log(airline);
+console.log(typeof airline);
+let typeAirline=prompt('which is the best type of airline');
+console.log(typeAirline);
+console.log(typeof typeAirline);
+alert('welcome to our site' + 'your name is'  + fullName + 'your age is' +  age + 'your favourite airline company' + airline + 'your best type of airline' + typeAirline )
